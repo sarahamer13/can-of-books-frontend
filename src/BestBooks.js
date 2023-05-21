11dc53a12b51720c915d8d04f104f27fff528851
Christina Hollie <christinahollie7@gmail.com>
2023-05-20 20:50:31 -0700

    added to front-end

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -7,9 +8,20 @@ class BestBooks extends React.Component {
       books: []
     }
   }
-  
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
 
+  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount() {
+    this.getBooks();
+  }
+
+  async getBooks() {
+    try {
+      const response = await axios.get('http://localhost:3001/getBooks');
+      this.setState({ books: response.data })
+    } catch (err) {
+      console.error(err);
+    }
+  }
   render() {
 
     /* TODO: render all the books in a Carousel */
@@ -18,11 +30,9 @@ class BestBooks extends React.Component {
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        {this.state.books.length > 0 && this.state.books.map((books, idx) => (
+          <h1>Book Carousel coming soon</h1>
+        ))}
       </>
     )
   }
