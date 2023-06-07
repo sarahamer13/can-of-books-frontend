aafb2f0bfbade252ece3220016efbb15c0940c1e
Christina Hollie <christinahollie7@gmail.com>
2023-06-06 22:13:42 -0700

    added login and logout and Auth0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 console.log('proof');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH_CLIENTID}
+      authorizationParams={{
+        redirect_uri: process.env.REACT_APP_REDIRECTURI
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
